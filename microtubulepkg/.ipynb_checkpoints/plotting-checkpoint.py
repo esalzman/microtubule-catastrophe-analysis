2f9f653f6c86f064a5fe1e4abd17977c9f436ee5
esalzman <salzman.erika@gmail.com>
2019-12-11 14:36:38 -0800

    Updated plotting

--- microtubulepkg/.ipynb_checkpoints/plotting-checkpoint.py
+++ microtubulepkg/.ipynb_checkpoints/plotting-checkpoint.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+import scipy.stats as st
 import bokeh_catplot
 
 import holoviews as hv
@@ -15,7 +17,8 @@ def plot_ecdf(tub_df):
         data=tub_df,
         cats=['concentration'],
         val='catastrophe time',
-        style='staircase'
+        style='staircase',
+        x_axis_label='catastrophe time (s)'
     )
 
     p.legend.location = 'bottom_right'
@@ -33,3 +36,20 @@ def plot_box(tub_df):
     )
 
     bokeh.io.show(box)
+    
+def plot_model_gamma(t, alpha_mle, beta_mle):
+    p = bokeh_catplot.ecdf(
+        pd.DataFrame({'t (s)': t}), 
+        val='t (s)', 
+        conf_int=True, 
+        x_axis_label='catastrophe time (s)',
+        #legend='Experimental data'
+    )
+
+    t_theor = np.linspace(0, 2000, 200)
+    cdf = st.gamma.cdf(t_theor, alpha_mle, loc=0, scale=1/beta_mle)
+    p.line(t_theor, cdf, line_width=2, color='orange', legend_label='Theoretical gamma distribution')
+    
+    p.legend.location = 'bottom_right'
+    
+    bokeh.io.show(p)
